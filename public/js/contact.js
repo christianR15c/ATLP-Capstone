bae9451a26c302a6c14d74eac062ab6b57488a58
christian <habinezac15@gmail.com>
2021-12-12 16:56:50 +0200

    new features added

---
+++ public/js/contact.js
@@ -0,0 +1,30 @@
+const button = document.getElementsByTagName('button')[0];
+const nameInput = document.querySelector('input[name="name"]');
+const emailInput = document.querySelector('input[type="email"]');
+const messageInput = document.querySelector('input[name="message"]');
+
+const validateInput = () => {
+    if(!nameInput.value){
+        nameInput.nextElementSibling.classList.remove('name-hidden');
+    }
+}
+
+const validateEmail = () => {
+    if(!emailInput.value.includes('@')){
+        emailInput.nextElementSibling.classList.remove('email-hidden');
+    }
+}
+
+const validateMessage = () => {
+    if(!messageInput.value){
+        messageInput.nextElementSibling.classList.remove('message-hidden');
+    }
+    
+}
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    validateInput();
+    validateEmail();
+    validateMessage();
+})
