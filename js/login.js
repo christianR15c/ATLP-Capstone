2d7f1ce73451bd6912cd9b7ced97bfb703c17075
christian <habinezac15@gmail.com>
2022-01-28 22:42:03 +0200

    article get request added

--- js/login.js
+++ js/login.js
@@ -1,26 +1,48 @@
 const button = document.querySelector('input[type="button"]');
 const emailInput = document.querySelector('input[type="text"]');
 const passswordInput = document.querySelector('input[type="password"]');
+const loader = document.querySelector('.loader');
 
-const validateEmail = () => {
-    if(!emailInput.value.includes('@')){
-        emailInput.nextElementSibling.classList.remove('hidden')
-    }
-}
-
-const validatePassword = () => {
-    if(!passswordInput.value){
-        passswordInput.nextElementSibling.classList.remove('hidden');
-    }
-}
+const validator = () => {
+  if (!emailInput.value.includes('@')) {
+    emailInput.nextElementSibling.classList.remove('hidden');
+  }
+  if (!passswordInput.value) {
+    passswordInput.nextElementSibling.classList.remove('hidden');
+  } else {
+    loader.classList.remove('loader-active');
+    fetch('https://mybrand-api.herokuapp.com/api/user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: emailInput.value,
+        password: passswordInput.value,
+      }),
+    })
+      .then((res) => res.json())
+      .then((loggedUser) => {
+        console.log(loggedUser);
+        if (!loggedUser.ok) {
+          swal('Failed!', 'Invalid username or password', 'error');
+          loader.classList.remove('loader-active');
+          resetForm();
+        } else {
+          console.log(loggedUser);
+          window.location.href = '../html/index.html';
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+};
 
 button.addEventListener('click', (e) => {
-    e.preventDefault();
-    validateEmail();
-    validatePassword();
-    resetForm();
+  e.preventDefault();
+  validator();
+  resetForm();
 });
 
 function resetForm() {
-    document.getElementById("form").reset();
+  document.getElementById('form').reset();
 }
--- js/signUp.js
+++ js/signUp.js
@@ -4,55 +4,63 @@ const emailInput = document.querySelector('input[type="email"]');
 const passswordInput = document.querySelector('input[type="password"]');
 const emailValidation = document.querySelector('div[id="hidden-email"]');
 const passwordValidation = document.querySelector('div[id="hidden-password"]');
+const loader = document.querySelector('.loader');
 
-const validateName = () => {
+const regexEmail = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
+const regexPassword = new RegExp(
+  '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{6,})'
+);
+const validator = () => {
   if (!nameInput.value) {
     nameInput.nextElementSibling.classList.remove('hidden');
-  }
-};
-
-const validateEmail = () => {
-  if (!emailInput.value.includes('@')) {
+  } else if (!emailInput.value.includes('@')) {
     emailInput.nextElementSibling.classList.remove('hidden');
-  }
-};
-
-const validatePassword = () => {
-  const regexEmail = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
-  const regexPassword = new RegExp(
-    '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{6,})'
-  );
-  if (!passswordInput.value) {
+  } else if (!passswordInput.value) {
     passswordInput.nextElementSibling.classList.remove('hidden');
   } else if (regexEmail.test(passswordInput.value)) {
     emailValidation.removeAttribute('id');
   } else if (regexPassword.test(passswordInput.value)) {
     passwordValidation.removeAttribute('id');
+  } else {
+    loader.classList.add('loader-active');
+    button.disabled = true;
+    console.log(nameInput.value);
+    fetch('https://mybrand-api.herokuapp.com/api/user/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: nameInput.value,
+        email: emailInput.value,
+        password: passswordInput.value,
+      }),
+    })
+      .then((addedUser) => {
+        console.log(addedUser);
+        if (addedUser.ok) {
+          swal('Thank you!', 'You have registered successfully', 'success');
+          loader.classList.remove('loader-active');
+          resetForm();
+        } else {
+          swal('Failed!', 'Registration not happened at this time', 'error');
+          loader.classList.remove('loader-active');
+          resetForm();
+        }
+      })
+      .catch((err) => console.log(err));
   }
 };
 
 button.addEventListener('click', (e) => {
   e.preventDefault();
-  validateName();
-  validateEmail();
-  //   validatePassword();
-
-  // creating a new user
-  fetch('https://mybrand-api.herokuapp.com/api/user/register', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      name: nameInput.textContent,
-      email: emailInput.textContent,
-      password: passswordInput.textContent,
-    }),
-  })
-    .then((addedUser) => console.log(addedUser))
-    .catch((err) => console.log(err));
+  validator();
 });
 
 function resetForm() {
   document.getElementById('form').reset();
 }
+// Swal.fire({
+//   icon: 'error',
+//   title: `failed`,
+// });
